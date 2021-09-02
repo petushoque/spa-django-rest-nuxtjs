4a3a8ece097c323641f5d1f1729212a95aad7618
Kubasov Alexei <kubasovalexei@yandex.ru>
2021-09-02 20:00:23 +0700

    Update section2lesson1step2.py

--- section2lesson1step2.py
+++ section2lesson1step2.py
@@ -2,3 +2,13 @@ x = input().split(',')
 print(x)
 y = int(input())
 print(y)
+res = []
+for i in range(len(x)):
+    for j in range(1, len(x)):
+        
+        if int(x[i]) + int(x[j]) == y:
+            print('yes')
+            res.append(x[i])
+            res.append(x[j])
+            break
+print(res)
