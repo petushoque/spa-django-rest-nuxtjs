95d2559522bec1879d323a1127907c9815b408b1
Kubasov Alexei <kubasovalexei@yandex.ru>
2021-09-02 20:26:25 +0700

    Update section2lesson1step3.py

--- section2lesson1step3.py
+++ section2lesson1step3.py
@@ -1,3 +1,24 @@
 #Напишите программу, которая находит самый длинный общий префикс для строк находящихся в списке строк
+#flower,flow,flight
 s = input().split(',')
+print(s)
+
+# найти самый короткий элемент
+min_elem = s[0]
+min_len = 100
+for i in range(len(s)):
+    if len(s[i]) < min_len:
+        min_len = len(s[i])
+        min_elem = s[i]
+
+print(min_elem, min_len)
+res = ''
+#for i in range(len(s[0])):
+#    for j in range(len(s[0])):
+#        counter = 0
+#        if s[0][i] == s[j][i]:
+#            counter += 1
+#        if counter == len(s):
+#            res += s[0][i]
+
 print(s)
