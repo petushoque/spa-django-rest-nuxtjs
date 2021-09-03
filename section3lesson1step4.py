5e10e5d411f9219881dca0bbb648eb9a4328986f
Kubasov Alexei <kubasovalexei@yandex.ru>
2021-09-03 11:24:17 +0700

    Update section3lesson1step4.py

--- section3lesson1step4.py
+++ section3lesson1step4.py
@@ -2,3 +2,20 @@ import roman;
 s = input()
 print(roman.fromRoman(s))
 #n=roman.fromRoman("X"); #n becomes 10
+
+def romanToInt(s):
+        result=0
+        f={'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
+        i=0
+        while i < len(s)-1:
+                if f[s[i+1]] > f[s[i]]:
+                        result+=f[s[i+1]]-f[s[i]]
+                        i+=2
+                else:
+                        result+=f[s[i]]
+                        i+=1
+        if i<len(s):
+                result+=f[s[len(s)-1]]
+        return result
+
+print(romanToInt('III'))
