ccb42b1f1f9b065613ff40cbc05118cfbb57be58
Kubasov Alexei <kubasovalexei@yandex.ru>
2021-09-03 11:33:48 +0700

    Update section3lesson1step5.py

--- section3lesson1step5.py
+++ section3lesson1step5.py
@@ -1,3 +1,10 @@
 x = int(input())
-x_rev = str(x)[::-1]
-print(x_rev)
+if x < 0:
+    x_rev = '-' + str(x)[:0:-1]
+    print(x_rev)
+if x > 0:
+    x_rev = str(x)[::-1]
+    if int(x_rev) < 2 ** 31:
+        print(x_rev)
+    else:
+        print(0)
