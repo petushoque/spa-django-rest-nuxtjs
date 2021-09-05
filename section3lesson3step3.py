5274a5dda7748400c392059b923384a65d8ff5c8
Kubasov Alexei <kubasovalexei@yandex.ru>
2021-09-05 08:48:03 +0700

    Update section3lesson3step3.py

--- section3lesson3step3.py
+++ section3lesson3step3.py
@@ -12,6 +12,10 @@ while len(x) or len(y) > 0:
     if len(y) == 0:
         res += x
         break
-    if x[0] < y[0]:
+    if x[0] <= y[0]:
         res += x[0]
-    
+        x.remove(x[0])
+    if y > x:
+        res += y[0]
+        y.remove(y[0])
+print(res)
