b1b031c7d97d205990a3f99591a345b80281e8e9
Kubasov Alexei <kubasovalexei@yandex.ru>
2021-09-05 09:47:32 +0700

    Update section3lesson3step3.py

--- section3lesson3step3.py
+++ section3lesson3step3.py
@@ -5,20 +5,21 @@ print(list(y))
 
 res = []
 
-while len(x) and len(y) > 0:
+while len(x) or len(y) > 0:
     print('before ', x, y)
     if len(x) == 0:
         print('yes')
         res.append(y)
         break
     if len(y) == 0:
-        res.append(x)
+        res += x
         break
     if int(x[0]) <= int(y[0]):
         res.append(x[0])
         x = x[1:]
-    if int(y[0]) < int(x[0]):
+    elif int(y[0]) < int(x[0]):
         res.append(y[0])
         y  = y[1:]
     print('after ', x, y)
+    print('result on this iteration', res)
 print(res)
