e6db415e602dd92e80d21c81181379d29b153c79
Kubasov Alexei <kubasovalexei@yandex.ru>
2021-09-05 09:19:15 +0700

    Update section3lesson3step3.py

--- section3lesson3step3.py
+++ section3lesson3step3.py
@@ -6,6 +6,7 @@ print(list(y))
 res = []
 
 while len(x) or len(y) > 0:
+    print(x, y)
     if len(x) == 0:
         print('yes')
         res.append(y)
@@ -13,10 +14,10 @@ while len(x) or len(y) > 0:
     if len(y) == 0:
         res.append(x)
         break
-    if x[0] <= y[0]:
+    if int(x[0]) <= int(y[0]):
         res.append(x[0])
-        x.remove(x[0])
-    if y[0] > x[0]:
+        del x[0]
+    if int(y[0]) > int(x[0]):
         res.append(y[0])
-        y.remove(y[0])
+        del y[0]
 print(res)
