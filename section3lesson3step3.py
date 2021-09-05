907e8090e074271050a540adb107fd45c9c50a8f
Kubasov Alexei <kubasovalexei@yandex.ru>
2021-09-05 08:44:37 +0700

    Update section3lesson3step3.py

--- section3lesson3step3.py
+++ section3lesson3step3.py
@@ -8,6 +8,10 @@ res = []
 while len(x) or len(y) > 0:
     if len(x) == 0:
         res += y
+        break
     if len(y) == 0:
         res += x
+        break
+    if x[0] < y[0]:
+        res += x[0]
     
