d165ce3b3c8e206b7da3b79032bf99e694f9b910
Kubasov Alexei <kubasovalexei@yandex.ru>
2021-09-05 09:40:47 +0700

    Update section3lesson3step3.py

--- section3lesson3step3.py
+++ section3lesson3step3.py
@@ -6,7 +6,7 @@ print(list(y))
 res = []
 
 while len(x) and len(y) > 0:
-    print(x, y)
+    print('before ', x, y)
     if len(x) == 0:
         print('yes')
         res.append(y)
@@ -20,4 +20,5 @@ while len(x) and len(y) > 0:
     if int(y[0]) < int(x[0]):
         res.append(y[0])
         y  = y[1:]
+    print('after ', x, y)
 print(res)
