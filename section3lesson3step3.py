adcbd525f58b6d0f2b188658d633a2978bf30f56
Kubasov Alexei <kubasovalexei@yandex.ru>
2021-09-05 09:53:42 +0700

    Update section3lesson3step3.py

--- section3lesson3step3.py
+++ section3lesson3step3.py
@@ -1,14 +1,10 @@
 x = input().split(',')
 y = input().split(',')
-print(list(x))
-print(list(y))
 
 res = []
 
 while len(x) or len(y) > 0:
-    print('before ', x, y)
     if len(x) == 0:
-        print('yes')
         res += y
         break
     if len(y) == 0:
@@ -20,6 +16,4 @@ while len(x) or len(y) > 0:
     elif int(y[0]) < int(x[0]):
         res.append(y[0])
         y  = y[1:]
-    print('after ', x, y)
-    print('result on this iteration', res)
 print(res)
