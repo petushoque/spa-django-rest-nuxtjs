dbb53f5a5f068d71816a631fa0223fdf59284d00
Kubasov Alexei <kubasovalexei@yandex.ru>
2021-09-05 08:43:29 +0700

    Update section3lesson3step3.py

--- section3lesson3step3.py
+++ section3lesson3step3.py
@@ -2,3 +2,12 @@ x = input().split(',')
 y = input().split(',')
 print(list(x))
 print(list(y))
+
+res = []
+
+while len(x) or len(y) > 0:
+    if len(x) == 0:
+        res += y
+    if len(y) == 0:
+        res += x
+    
