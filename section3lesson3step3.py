4fc761b2d6e36a02ef673fed0abbea1817589ad5
Kubasov Alexei <kubasovalexei@yandex.ru>
2021-09-05 09:00:29 +0700

    Update section3lesson3step3.py

--- section3lesson3step3.py
+++ section3lesson3step3.py
@@ -7,15 +7,16 @@ res = []
 
 while len(x) or len(y) > 0:
     if len(x) == 0:
-        res += y
+        print('yes')
+        res.append(y)
         break
     if len(y) == 0:
-        res += x
+        res.append(x)
         break
     if x[0] <= y[0]:
-        res += x[0]
+        res.append(x[0])
         x.remove(x[0])
-    if y > x:
-        res += y[0]
+    if y[0] > x[0]:
+        res.append(y[0])
         y.remove(y[0])
 print(res)
