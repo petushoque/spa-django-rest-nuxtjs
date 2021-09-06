0874b43b7e99f9572a1b5a58f917d55e66538d49
Kubasov Alexei <kubasovalexei@yandex.ru>
2021-09-06 08:50:32 +0700

    Update section3lesson3step4.py

--- section3lesson3step4.py
+++ section3lesson3step4.py
@@ -2,5 +2,5 @@ nums = input().split(',')
 res = [int(nums[0])]
 for i in range(1, len(nums)):
     if int(nums[i]) != int(nums[i - 1]):
-        res.append(int(num[i]))
-print(nums)
+        res.append(int(nums[i]))
+print(res)
