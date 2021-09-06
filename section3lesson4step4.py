4e3a4c0322e3dafb40a95398fe11e87acf8fc99a
Kubasov Alexei <kubasovalexei@yandex.ru>
2021-09-06 09:16:41 +0700

    Update section3lesson4step4.py

--- section3lesson4step4.py
+++ section3lesson4step4.py
@@ -2,5 +2,8 @@ nums = [int(i) for i in input().split(',')]
 x = int(input())
 print(nums, x)
 
+tmp = nums[0]
 for i in range(len(nums)):
-    
+    if x == nums[i]:
+        print(i)
+        break
