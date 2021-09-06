8f04656b425fdb48df1333d6302c7244bc2dcafa
Kubasov Alexei <kubasovalexei@yandex.ru>
2021-09-06 09:24:01 +0700

    Update section3lesson4step4.py

--- section3lesson4step4.py
+++ section3lesson4step4.py
@@ -1,16 +1,19 @@
 nums = [int(i) for i in input().split(',')]
 x = int(input())
-print(nums, x)
+
+is_in_list = False
 
 tmp = nums[0]
 tmp_ind = 0
 for i in range(len(nums)):
     if x == nums[i]:
         print(i)
+        is_in_list = True
         break
     elif nums[i] < x:
         tmp = nums[i]
         tmp_ind = i
 
-#вывести индекс + 1        
-print(tmp, tmp_ind)
+if not is_in_list:
+    #вывести индекс + 1        
+    print(tmp, tmp_ind)
