d9b2e67e80c95e45727480006ef77896957eafe7
Kubasov Alexei <kubasovalexei@yandex.ru>
2021-09-06 09:21:48 +0700

    Update section3lesson4step4.py

--- section3lesson4step4.py
+++ section3lesson4step4.py
@@ -3,7 +3,13 @@ x = int(input())
 print(nums, x)
 
 tmp = nums[0]
+tmp_ind = 0
 for i in range(len(nums)):
     if x == nums[i]:
         print(i)
         break
+    elif nums[i] < x:
+        tmp = nums[i]
+        tmp_ind = i
+        
+print(tmp, tmp_ind)
