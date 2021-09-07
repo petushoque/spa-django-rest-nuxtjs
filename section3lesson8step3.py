70e80c795a6d9451414cd8065050bd89f3a130cc
Kubasov Alexei <kubasovalexei@yandex.ru>
2021-09-07 19:51:08 +0700

    Update section3lesson8step3.py

--- section3lesson8step3.py
+++ section3lesson8step3.py
@@ -2,7 +2,6 @@
 #Верните самый часто повторяемый элемент из списка. Часто повторяемый это элемент, который появляется больше чем [n/2]
 
 nums = [int(i) for i in  input().split(',')]
-print(nums)
 
 pop_num = 0
 pop_num_count = 0
@@ -10,5 +9,9 @@ pop_num_count = 0
 for i in range(len(nums)):
     if nums.count(nums[i]) > pop_num_count:
         pop_num = nums[i]
-        pop_num_count = nums.count(i)
-print(pop_num, pop_num_count)
+        pop_num_count = nums.count(nums[i])
+
+#print(pop_num, pop_num_count)
+
+if pop_num_count > (len(nums) / 2):
+    print(pop_num)
