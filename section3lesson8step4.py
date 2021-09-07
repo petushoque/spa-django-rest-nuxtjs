1b344814faf0bd9d596949dda3215618a2a730d1
Kubasov Alexei <kubasovalexei@yandex.ru>
2021-09-07 19:59:32 +0700

    Update section3lesson8step4.py

--- section3lesson8step4.py
+++ section3lesson8step4.py
@@ -1,4 +1,14 @@
 #Дан массив чисел. Если в нем есть дубликаты, то верните True, если нет, то False.
 
 nums = [int(i) for i in  input().split(',')]
-print(nums)
+
+has_duble = False
+
+for i in range(len(nums)):
+    if nums.count(nums[i]) > 1:
+        has_double = True
+        
+if has_double:
+    print('True')
+else:
+    print('False')
