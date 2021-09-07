7b462bae068487829fefd76a5d85ccc966710b78
Kubasov Alexei <kubasovalexei@yandex.ru>
2021-09-07 19:49:00 +0700

    Update section3lesson8step3.py

--- section3lesson8step3.py
+++ section3lesson8step3.py
@@ -3,3 +3,12 @@
 
 nums = [int(i) for i in  input().split(',')]
 print(nums)
+
+pop_num = 0
+pop_num_count = 0
+
+for i in range(len(nums)):
+    if nums.count(nums[i]) > pop_num_count:
+        pop_num = nums[i]
+        pop_num_count = nums.count(i)
+print(pop_num, pop_num_count)
