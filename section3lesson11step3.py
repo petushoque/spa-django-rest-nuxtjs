740eceac8af8aac1b9df93d3172f2121ee232435
Kubasov Alexei <kubasovalexei@yandex.ru>
2021-09-08 10:54:00 +0700

    Update section3lesson11step3.py

--- section3lesson11step3.py
+++ section3lesson11step3.py
@@ -1,3 +1,13 @@
 # Дана строка. Найдите в ней все глассные и разверни их в обратную строну.
 word = input()
 print(word)
+
+vowels = ['a', 'e', 'i', 'o', 'u', 'y']
+
+vowels_in_word = []
+
+for i in range(len(word)):
+    if word[i] in vowels:
+        vowels_in_word.append(word[i])
+        
+print(vowels_in_word)
