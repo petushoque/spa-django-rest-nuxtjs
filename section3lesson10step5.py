f6ad58d46f27c9c86c66318f0520f9616b2cf37e
Kubasov Alexei <kubasovalexei@yandex.ru>
2021-09-08 09:40:27 +0700

    Update section3lesson10step5.py

--- section3lesson10step5.py
+++ section3lesson10step5.py
@@ -7,4 +7,10 @@ zero_counter = 0
 
 for i in range(len(nums)):
     if nums[i] != 0:
+        res.append(nums[i])
+
+if zero_counter > 0:
+    for i in range(zero_counter):
+        res.append(0)
         
+print(res)
