b7ae9a8e1a720e3cef75134a841be8363e002979
Kubasov Alexei <kubasovalexei@yandex.ru>
2021-09-08 10:55:51 +0700

    Update section3lesson11step3.py

--- section3lesson11step3.py
+++ section3lesson11step3.py
@@ -5,9 +5,15 @@ print(word)
 vowels = ['a', 'e', 'i', 'o', 'u', 'y']
 
 vowels_in_word = []
+result = []
+counter = 0
 
 for i in range(len(word)):
     if word[i] in vowels:
         vowels_in_word.append(word[i])
         
 print(vowels_in_word)
+
+for i in range(len(word)):
+    if word[i] in vowels:
+        
