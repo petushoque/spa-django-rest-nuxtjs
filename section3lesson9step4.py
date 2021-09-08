9d95e2fd9d010f1a9c649f0a6f940f31d6d64abe
Kubasov Alexei <kubasovalexei@yandex.ru>
2021-09-08 07:34:05 +0700

    Update section3lesson9step4.py

--- section3lesson9step4.py
+++ section3lesson9step4.py
@@ -1,13 +1,10 @@
 #Дан массив чисел nums и число k. Определите есть ли в массиве такие числа дубликаты, чтобы между ними было расстояние k.
 nums = [int(i) for i in  input().split(',')]
-print(nums)
 k = int(input())
-print(k)
 
 has_double = False
 
 for i in range(k + 1, len(nums)):
-    print(nums[i], nums[i - (k + 1)])
     if nums[i] == nums[i - k]:
         has_double = True
         
