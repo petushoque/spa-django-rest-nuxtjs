574fec9cb5cee7175dab1b329a0db9e998a4f428
Kubasov Alexei <kubasovalexei@yandex.ru>
2021-09-10 08:42:30 +0700

    Update views.py

--- spa/spaapplication/views.py
+++ spa/spaapplication/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import View
+
+class MainView(View):
+    def get(self, request, *args, **kwargs):
+        return render(
+            request,
+            'myblog/index.html'
+        )
 
 # Create your views here.
