08152f2e9a5597a4bfb130994f05a2a3990fe940
Kubasov Alexei <kubasovalexei@yandex.ru>
2021-09-10 08:52:23 +0700

    Update urls.py

--- spa/spa/urls.py
+++ spa/spa/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-    path('', include('myblog.urls')),
+    path('', include('spaapplication.urls')),
     path('admin/', admin.site.urls),
 ]
